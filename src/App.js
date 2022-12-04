eab9e8e94a73f476d90522f82fc85e11e3097ef6
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-12-03 20:22:53 -0700

    hover and mouse effects

--- src/App.js
+++ src/App.js
@@ -17,24 +17,58 @@ function App() {
             panY = gallery.offsetHeight * yDecimal * -1 * 0.5;
       // set up translate
 
-      console.log(mouseX,mouseY);
-      console.log(panX,panY);
-      gallery.style.transform = `translate(${panX}px,${panY}px)`
+      gallery.animate({
+        transform:`translate(${panX}px,${panY}px)`
+      },{
+        duration:4000,
+        fill:"forwards",
+        easing:"ease"
+      })
     }
+    
   },[]);
   return (
     <div className="App">
       <div id="gallery">
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
-        <div class="tile"></div>
+        <div class="tile">
+          <img src="https://images.unsplash.com/photo-1670022326053-74ee3cdc1cc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+        </div>
+        <div class="tile">
+        <img src="https://images.unsplash.com/photo-1670022326053-74ee3cdc1cc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+
+        </div>
+        <div class="tile">
+                    <img src="https://images.unsplash.com/photo-1670022326053-74ee3cdc1cc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+
+        </div>
+        <div class="tile">
+                    <img src="https://images.unsplash.com/photo-1669989482567-1590b25adbdd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+
+        </div>
+        <div class="tile">
+                    <img src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1686&q=80" />
+
+        </div>
+        <div class="tile">
+                    <img src="https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+
+        </div>
+        <div class="tile">
+                    <img src="https://images.unsplash.com/photo-1495360010541-f48722b34f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80" />
+
+        </div>
+        <div class="tile">
+                    <img src="https://images.unsplash.com/photo-1670022326053-74ee3cdc1cc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+
+        </div>
+        <div class="tile">
+                    <img src="https://images.unsplash.com/photo-1670022326053-74ee3cdc1cc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+
+        </div>
+        <div class="tile">
+                    <img src="https://images.unsplash.com/photo-1670022326053-74ee3cdc1cc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+
+        </div>
 
       </div>
     </div>
