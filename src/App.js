02dee894fa7ce45bfc4bc00c886c6b2b173ffcf0
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-12-03 20:53:14 -0700

    modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
-import { useEffect } from "react";
-
+import { useEffect,useState } from "react";
+import Modal from "@mui/material/Modal";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 function App() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
   useEffect(() => {
     const gallery = document.getElementById("gallery");
     window.onmousemove = (e) => {
@@ -31,10 +48,26 @@ function App() {
 
   function doModule(e) {
     console.log(e.target.getAttribute("data-nav"));
+    handleOpen();
   }
 
   return (
     <div className="App">
+<Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+    <Typography id="modal-modal-title" variant="h6" component="h2">
+      Text in a modal
+    </Typography>
+    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+    </Typography>
+  </Box>
+</Modal>      
       <div id="gallery">
         <div class="tile">
           <img  onClick={doModule} data-nav="1" src="https://images.unsplash.com/photo-1670022326053-74ee3cdc1cc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"/>
